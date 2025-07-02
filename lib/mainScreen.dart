7c90084843adb6e6db3f37f61ec157ca5ee83591
Furkan Yaman <77271600+ffurkan04@users.noreply.github.com>
2025-07-02 12:51:29 +0300

    Yorum satırları eklendi

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -3,26 +3,37 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
+  //Stateful Widget, Türkçeye çevirirsek durum alabilen Widget olarak çevirebiliriz
+  //Bu bizim sayfada olan değişiklikleri sayfayı yenilemeden yapmamızı sağlar.
+  //Bu widget kullanıcıdan veri aldığımız ekranlar için kullanılır.
+  //Bu sayfada veri alacağımız kısım arama kutusudur.
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
+  //Tamamen örnek olarak burada bir liste atayıp oradan ilaçların isimlerini çekiyoruz.
+  //Daha sonrasında veri tabanınından alacağız bu verileri.
   List<String> mediciences = ["Example1", "Example2"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My Mediciences")),
+      appBar: AppBar(
+        title: const Text("My Mediciences"),
+      ), //Sayfanın en üstüne görünen kısım.
       body: ListView.builder(
         itemCount: mediciences.length,
         itemBuilder: (context, index) {
           return Padding(
+            //Burada ilaçların isminin yazdığı text ile genel ilaçların olduğu kutunun
+            //arasındaki boşluğu belirşiyoruz.
             padding: const EdgeInsets.symmetric(
               horizontal: 12.0,
               vertical: 6.0,
             ),
             child: Card(
+              //İlaçların isimlerine dikdörtgensi bir görünüm veriyoruz.
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12),
               ),
